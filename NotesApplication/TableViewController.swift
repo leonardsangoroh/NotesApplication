91931ef59c1a5b3ef14b741c083437199ed3e4ad
Leonard Sangoroh <61079370+leonardsangoroh@users.noreply.github.com>
2024-03-18 17:37:37 +0300

    feat: create table view controller that lists notes & place it inside a nav controller

--- NotesApplication/TableViewController.swift
+++ NotesApplication/TableViewController.swift
@@ -8,12 +8,28 @@
 import UIKit
 
 class TableViewController: UITableViewController {
+    
+    var contents = ["Notes1", "Notes2", "Notes3"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        view.backgroundColor = .systemRed
+        
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+      return contents.count
     }
 
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+      let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+      cell.textLabel?.text = contents[indexPath.row]
+      return cell
+    }
     
 }
 
