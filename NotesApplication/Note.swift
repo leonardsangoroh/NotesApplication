7ffab4384086a05ea138ae0aca0686664a2cd06d
Leonard Sangoroh <61079370+leonardsangoroh@users.noreply.github.com>
2024-03-19 11:15:44 +0300

    feat: implement 'add' nav button and implement save notes using UserDefaults

---
+++ NotesApplication/Note.swift
@@ -0,0 +1,13 @@
+//
+//  Note.swift
+//  NotesApplication
+//
+//  Created by Lee Sangoroh on 19/03/2024.
+//
+
+import Foundation
+
+struct Note: Codable {
+    var heading: String
+    var noteContent: String?
+}
--- NotesApplication/TableViewController.swift
+++ NotesApplication/TableViewController.swift
@@ -9,15 +9,65 @@ import UIKit
 
 class TableViewController: UITableViewController {
     
-    var contents = ["Notes1", "Notes2", "Notes3"]
+    var contents = [Note]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         view.backgroundColor = .systemRed
         
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Add", style: .plain, target: self, action: #selector(addNote))
+        
         tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
         
+        ///load array from disk when app runs
+        let defaults = UserDefaults.standard
+        
+        ///get optional Data
+        if let savedNotes = defaults.object(forKey: "notes") as? Data {
+            let jsonDecoder = JSONDecoder()
+            
+            do {
+                ///give it to jsonDecoder to convert it back to object graph
+                contents = try jsonDecoder.decode([Note].self, from: savedNotes)
+            } catch {
+                print("Failed to load people")
+            }
+        }
+        
+    }
+    
+    
+    @objc func addNote(){
+        let ac = UIAlertController(title: "Add Note", message: nil, preferredStyle: .alert)
+        ac.addTextField()
+        let submitAction = UIAlertAction(title: "New Note", style: .default) { [weak self, weak ac] _ in
+            guard let item = ac?.textFields?[0].text else {return}
+            self?.submit(item)
+            
+        }
+        
+        ac.addAction(submitAction)
+        present(ac, animated: true)
+        
+    }
+    
+    func submit(_ item: String){
+        let note = Note(heading: item, noteContent: "")
+        contents.insert(note, at: 0)
+        let indexPath = IndexPath(row: 0, section: 0)
+        tableView.insertRows(at: [indexPath], with: .automatic)
+        save()
+        return
+    }
+    
+    
+    func save () {
+        let jsonEncoder = JSONEncoder()
+        if let savedData = try? jsonEncoder.encode(contents) {
+            let defaults = UserDefaults.standard
+            defaults.set(savedData, forKey: "notes")
+        }
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -26,15 +76,18 @@ class TableViewController: UITableViewController {
 
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-      let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-      cell.textLabel?.text = contents[indexPath.row]
-      return cell
+        
+        let note = contents[indexPath.item]
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = note.heading
+        return cell
     }
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        
+        let note = contents[indexPath.item]
         let vc = DetailViewController()
-        vc.noteIdentifier = contents[indexPath.row]
+        vc.noteIdentifier = note.heading
         print(vc.noteIdentifier)
         self.navigationController?.pushViewController(vc, animated: true)
     }
